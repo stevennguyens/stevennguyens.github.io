364bbb4bbab9cc75240689d0f4d93d96b3d989b8
stevennguyens <stevennguyyenz@gmail.com>
2022-12-02 18:44:49 -0800

    change background/font color

--- script.js
+++
@@ -1,4 +0,0 @@
-const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
-const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
-console.log(vw)
-console.log(vh)
