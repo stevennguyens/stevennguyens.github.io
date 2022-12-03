db5706a0c21adb88224a1cfe5644067f2290b2cd
stevennguyens <stevennguyyenz@gmail.com>
2022-12-02 17:24:38 -0800

    paintings / contact

---
+++ script.js
@@ -0,0 +1,4 @@
+const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
+console.log(vw)
+console.log(vh)
