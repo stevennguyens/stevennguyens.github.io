69b849e665477f5a4135766e3b14460bd073843c
stevennguyens <stevennguyyenz@gmail.com>
2022-12-03 18:38:01 -0800

    donezo

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // intersection observer
 let options = {
-    rootMargin: '-10%',
+    rootMargin: '0%',
     threshold: 0.0,
 }
 
@@ -10,9 +10,7 @@ function showContent(entries) {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
             entry.target.classList.add('show');
-        } else {
-            entry.target.classList.remove('show');
-        }
+        } 
     })
 }
 
@@ -20,26 +18,37 @@ function handleScroll() {
     let scrollY = window.scrollY;
     let x;
     let y;
+    let ratioX;
+    let ratioY;
+    let newX;
     let newY;
+    let top;
+    let right;
     paintings.forEach((painting, i) => {
         x = parseInt(painting.style.getPropertyValue("--x"));
         y = parseInt(painting.style.getPropertyValue("--y"));
-        let ratioX = painting.dataset.ratioX ? painting.dataset.ratioX : 0;
-        let ratioY = painting.dataset.ratioY ? painting.dataset.ratioY : 0;
+        ratioX = painting.dataset.ratioX ? painting.dataset.ratioX : 0;
+        ratioY = painting.dataset.ratioY ? painting.dataset.ratioY : 0;
         // go from top to bottom else bottom to top
-        let top = painting.dataset.dirY === "top" ? true : false;
+        top = painting.dataset.dirY === "top" ? true : false;
         // go from right to left else left to right
-        let right = painting.dataset.dirX === "right" ? true : false;
+        right = painting.dataset.dirX === "right" ? true : false;
         // calculate new x position
         newX = right ? x - (scrollY * ratioX) : x + (scrollY * ratioX);
         // calculate new y position
         newY = top ? y + (scrollY * ratioY) : y - (scrollY * ratioY);
         painting.style.transform = `translate(${newX}px, ${newY}px)`;
     })
-    console.log("scroll")
+    
 }
 
 let paintings = document.querySelectorAll(".painting");
 window.addEventListener("scroll", handleScroll)
 let sections = document.querySelectorAll("section");
 sections.forEach((section) => observer.observe(section));
+
+let projectTitle = document.getElementById('project-header');
+let newString = '';
+let text = projectTitle.innerHTML.split('');
+text.map(letter => newString += `<span>${letter}</span>`);
+projectTitle.innerHTML = newString;
