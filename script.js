f2d3a4e1371fd148193d095b3a3ced24e997c4ea
stevennguyens <stevennguyyenz@gmail.com>
2022-12-03 16:27:26 -0800

    finished contact page
    painting transitions

--- script.js
+++ script.js
@@ -10,13 +10,36 @@ function showContent(entries) {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
             entry.target.classList.add('show');
-        } 
+        } else {
+            entry.target.classList.remove('show');
+        }
     })
 }
 
-let sections = document.querySelectorAll("section");
-sections.forEach((section) => observer.observe(section))
-// transitions for paintings
-// let paintings = document.querySelectorAll("painting");
+function handleScroll() {
+    let scrollY = window.scrollY;
+    let x;
+    let y;
+    let newY;
+    paintings.forEach((painting, i) => {
+        x = parseInt(painting.style.getPropertyValue("--x"));
+        y = parseInt(painting.style.getPropertyValue("--y"));
+        let ratioX = painting.dataset.ratioX ? painting.dataset.ratioX : 0;
+        let ratioY = painting.dataset.ratioY ? painting.dataset.ratioY : 0;
+        // go from top to bottom else bottom to top
+        let top = painting.dataset.dirY === "top" ? true : false;
+        // go from right to left else left to right
+        let right = painting.dataset.dirX === "right" ? true : false;
+        // calculate new x position
+        newX = right ? x - (scrollY * ratioX) : x + (scrollY * ratioX);
+        // calculate new y position
+        newY = top ? y + (scrollY * ratioY) : y - (scrollY * ratioY);
+        painting.style.transform = `translate(${newX}px, ${newY}px)`;
+    })
+    console.log("scroll")
+}
 
-// window.addEventListener("scr")
+let paintings = document.querySelectorAll(".painting");
+window.addEventListener("scroll", handleScroll)
+let sections = document.querySelectorAll("section");
+sections.forEach((section) => observer.observe(section));
