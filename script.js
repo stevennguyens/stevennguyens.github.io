6f3fab0bcdafffc1661e92a115c39634bbf16827
stevennguyens <stevennguyyenz@gmail.com>
2023-05-22 18:58:51 -0700

    added projects

--- script.js
+++ script.js
@@ -43,7 +43,7 @@ function handleScroll() {
     let greetings = document.querySelector('.greetings');
     let about = document.querySelector('.about');
     // let projectTitle = document.getElementById('project-header');
-    greetings.style.transform = `translateY(${scrollY < 600 ? scrollY / 8 : 0}%)`;
+    greetings.style.transform = `translateY(${scrollY < 600 ? scrollY / 12 : 0}%)`;
     about.style.transform = `translateY(${scrollY < 800 ? -scrollY / 10 : 0}%)`;
     // projectTitle.style.transform = `translateY(${(scrollY < 1400) ? -scrollY / 30 : 0}%)`;
 
