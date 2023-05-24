6d2c73cdfcc236afd3d43ee7fa2834928df09372
stevennguyens <stevennguyyenz@gmail.com>
2023-05-23 19:50:20 -0700

    added media queries for mobile support

--- script.js
+++ script.js
@@ -14,37 +14,14 @@ function showContent(entries) {
     })
 }
 
+let paintingRatio = 40
 function handleScroll() {
-    let scrollY = window.scrollY;
-    let x;
-    let y;
-    let ratioX;
-    let ratioY;
-    let newX;
-    let newY;
-    let top;
-    let right;
-    
-    x = parseInt(painting.style.getPropertyValue("--x"));
-    y = parseInt(painting.style.getPropertyValue("--y"));
-    ratioX = painting.dataset.ratioX ? painting.dataset.ratioX : 0;
-    ratioY = painting.dataset.ratioY ? painting.dataset.ratioY : 0;
-    // go from top to bottom else bottom to top
-    top = painting.dataset.dirY === "top" ? true : false;
-    // go from right to left else left to right
-    right = painting.dataset.dirX === "right" ? true : false;
-    // calculate new x position
-    newX = right ? x - (scrollY * ratioX) : x + (scrollY * ratioX);
-    // calculate new y position
-    newY = top ? y + (scrollY * ratioY) : y - (scrollY * ratioY);
-    painting.style.transform = `translate(${newX}px, ${newY}px)`;
+    painting.style.transform = `translateY(${scrollY > 1200 ? scrollY / paintingRatio : 0}%)`;
 
     let greetings = document.querySelector('.greetings');
     let about = document.querySelector('.about');
-    // let projectTitle = document.getElementById('project-header');
     greetings.style.transform = `translateY(${scrollY < 600 ? scrollY / 12 : 0}%)`;
     about.style.transform = `translateY(${scrollY < 800 ? -scrollY / 10 : 0}%)`;
-    // projectTitle.style.transform = `translateY(${(scrollY < 1400) ? -scrollY / 30 : 0}%)`;
 }
 
 let painting = document.querySelector(".contact-painting .painting");
@@ -52,8 +29,39 @@ window.addEventListener("scroll", handleScroll)
 let sections = document.querySelectorAll("section");
 sections.forEach((section) => observer.observe(section));
 
-// let projectTitle = document.getElementById('project-header');
-// let newString = '';
-// let text = projectTitle.innerHTML.split('');
-// text.map(letter => newString += `<span>${letter}</span>`);
-// projectTitle.innerHTML = newString;
+// media query
+var media = window.matchMedia("(max-width: 1024px)");
+var menu = document.getElementById("menu");
+var sidebar = document.querySelector(".sidebar");
+var sidebarUl = sidebar.querySelectorAll("ul");
+menu.addEventListener("click", (e) => {
+    toggleMenu(menu)
+})
+
+// make menu visible when media is phone or tablet (768px or less)
+if (media.matches) {
+    menu.classList.remove("no-display");
+    paintingRatio = 30;
+    painting.querySelector("img").width = "400";
+    sidebarUl.forEach((ul) => {
+        ul.classList.add("no-display");
+        ul.querySelectorAll("li").forEach(li => {
+            li.addEventListener("click", () => {
+                toggleMenu(menu)
+            })
+        })
+    });
+}
+
+// toggle menu 
+function toggleMenu(menu) {
+    if (menu.innerHTML.trim() === "menu") {
+        menu.innerHTML = "close";
+        sidebarUl.forEach((ul) => ul.classList.remove("no-display"));
+        sidebar.classList.add("active");
+    } else {
+        menu.innerHTML = "menu";
+        sidebarUl.forEach((ul) => ul.classList.add("no-display"));
+        sidebar.classList.remove("active");
+    }
+}
